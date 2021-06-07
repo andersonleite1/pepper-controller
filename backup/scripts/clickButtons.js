d323367ca569b243cb7f5574f855affa07f0918d
Anderson Leite <anderson.leite.bsi@gmail.com>
2021-06-07 08:41:32 -0300

    Add paste bkp

---
+++ backup/scripts/clickButtons.js
@@ -0,0 +1,27 @@
+// function keyPressed(evt){
+//     evt = evt || window.event;
+//     var key = evt.keyCode || evt.which;
+//     return String.fromCharCode(key); 
+// }
+
+// pressed.onkeypress = function(evt) {
+//     var str = keyPressed(evt);
+    
+//     if(str == 'f')
+//         alert("Apertou o 'f', chamando uma função...");
+//     };
+    
+    
+    
+//     var pressed = document.getElementById('pressed');
+// function keyPressed(evt){
+//     evt = evt || window.event;
+//     var key = evt.keyCode || evt.which;
+//     return String.fromCharCode(key); 
+// }
+
+// document.onkeypress = function(evt) {
+//     var str = keyPressed(evt);
+//     pressed.innerHTML += str;
+// };
+
